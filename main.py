9b59da222a676397b4b7ee65dde60504de6b5bb9
John Doe <johndoe@example.com>
2023-01-06 17:47:34 +0200

    br

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import random as ran
 import PySimpleGUI as sg
-sg.theme('DarkAmber') 
+sg.theme('DarkAmber')
+bruh = ran. 
+bruh2 = [1,2,3,4,5,6,7,8,9,0]
 layout = [
-    [sg.Button("press here to start"),sg.InputText()]
+    [sg.Text() ,sg.Button("press here to start"),sg.InputText()]
 ]
 
 window = sg.Window("title",layout)
@@ -9,3 +12,4 @@ while True:
     event,values = window.read()
     if event == sg.WIN_CLOSED:
         break
+    if event == 
