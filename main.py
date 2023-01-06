5d4b9873afe77d04d9d0d63cdcf192aa218eb50f
John Doe <johndoe@example.com>
2023-01-06 17:38:43 +0200

    bru

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import PySimpleGUI as sg
+sg.theme('DarkAmber') 
+layout = [
+    [sg.Button("press here to start"),sg.InputText()]
+]
 
-window = sg.Window("title",[[sg.Text("Sup world")]])
-window.read()
-##test
+window = sg.Window("title",layout)
+while True:
+    event,values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
