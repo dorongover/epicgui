ca685ae8a35f535ff893e189f31b1497071ac9c5
Omri <67146455+omriberchman@users.noreply.github.com>
2023-01-06 22:08:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import random
-import random as ran
 import PySimpleGUI as sg
 sg.theme('DarkAmber')
 
 word_ls = ['develop','work','make','tomorrow','control','visual','computer','interest','bruh']
 
 
+def combine_sentence(word_ls):
+  sentence = ""
+  for i in range(len(word_ls)):
+    sentence += word_ls[i] + " "
+  return sentence
 
-for i in range(6):
-    sent = random.choice(word_ls)
-    print(sent)
+
+sentence = combine_sentence(random.sample(word_ls, len(word_ls)))
 
 layout = [
+    [sg.Text(sentence)],
     [sg.Button("press here to start",key="field"),sg.InputText(key='text')]
 ]
 
 window = sg.Window("title",layout)
-
+win = False
 while True:
     event,values = window.read()
     if event == sg.WIN_CLOSED:
         break
     if event == "field":
         t = values['text']
-        print(t)
+        if t == sentence:
+            win = True
+        break
